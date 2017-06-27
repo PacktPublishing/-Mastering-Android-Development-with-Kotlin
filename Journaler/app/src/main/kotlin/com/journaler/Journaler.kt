8d9b953bb04cb157770a5d3be96307afe430a9e0
Milos Vasic <milos85vasic@gmail.com>
2017-06-27 11:10:32 +0200

    Journaler: Exploring logging.

--- Journaler/app/src/main/kotlin/com/journaler/Journaler.kt
+++ Journaler/app/src/main/kotlin/com/journaler/Journaler.kt
@@ -2,17 +2,30 @@ package com.journaler
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
 
 
 class Journaler : Application() {
 
     companion object {
+        val tag = "Journaler"
         var ctx: Context? = null
     }
 
     override fun onCreate() {
         super.onCreate()
         ctx = applicationContext
+        Log.v(tag, "[ ON CREATE ]")
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        Log.w(tag, "[ ON LOW MEMORY ]")
+    }
+
+    override fun onTrimMemory(level: Int) {
+        super.onTrimMemory(level)
+        Log.d(tag, "[ ON TRIM MEMORY ]: $level")
     }
 
 }
