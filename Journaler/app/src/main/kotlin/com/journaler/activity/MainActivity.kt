003ad8097e88c64f4af156b44870187191db09ce
Milos Vasic <milos85vasic@gmail.com>
2017-06-27 11:16:32 +0200

    Journaler: Exploring logging with activity lifecycle.

--- Journaler/app/src/main/kotlin/com/journaler/activity/MainActivity.kt
+++ Journaler/app/src/main/kotlin/com/journaler/activity/MainActivity.kt
@@ -3,13 +3,54 @@ package com.journaler.activity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.journaler.Journaler
 import com.journaler.R
 
 
 class MainActivity : AppCompatActivity() {
 
+    private val tag = Journaler.tag
+
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_main)
+        Log.v(tag, "[ ON CREATE ]")
     }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        Log.v(tag, "[ ON POST CREATE ]")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v(tag, "[ ON RESUME ]")
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+        Log.v(tag, "[ ON POST RESUME ]")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v(tag, "[ ON START ]")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(tag, "[ ON STOP ]")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v(tag, "[ ON PAUSE ]")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(tag, "[ ON DESTROY ]")
+    }
+
 }
