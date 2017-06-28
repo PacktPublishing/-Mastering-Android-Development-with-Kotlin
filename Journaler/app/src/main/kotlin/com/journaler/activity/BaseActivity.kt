865d0b1914fab56a0cecda7c2f5db221b725c36d
Milos Vasic <milos85vasic@gmail.com>
2017-06-28 10:24:56 +0200

    Journaler: Refactoring.

---
+++ Journaler/app/src/main/kotlin/com/journaler/activity/BaseActivity.kt
@@ -0,0 +1,61 @@
+package com.journaler.activity
+
+import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+import android.util.Log
+
+
+abstract class BaseActivity : FragmentActivity() {
+
+    protected abstract val tag : String
+
+    protected abstract fun getLayout(): Int
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayout())
+        Log.v(tag, "[ ON CREATE ]")
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        Log.v(tag, "[ ON POST CREATE ]")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v(tag, "[ ON RESTART ]")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.v(tag, "[ ON START ]")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v(tag, "[ ON RESUME ]")
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+        Log.v(tag, "[ ON POST RESUME ]")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v(tag, "[ ON PAUSE ]")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(tag, "[ ON STOP ]")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(tag, "[ ON DESTROY ]")
+    }
+
+}
--- Journaler/app/src/main/kotlin/com/journaler/activity/MainActivity.kt
+++ Journaler/app/src/main/kotlin/com/journaler/activity/MainActivity.kt
@@ -1,67 +1,12 @@
 package com.journaler.activity
 
-import android.os.Bundle
-import android.os.PersistableBundle
-import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import com.journaler.Journaler
 import com.journaler.R
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
-    private val tag = Journaler.tag
+    override val tag = "Main activity"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.v(tag, "[ ON CREATE 1 ]")
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        Log.v(tag, "[ ON CREATE 2 ]")
-    }
-
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-        Log.v(tag, "[ ON POST CREATE ]")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.v(tag, "[ ON RESTART ]")
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        Log.v(tag, "[ ON START ]")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.v(tag, "[ ON RESUME ]")
-    }
-
-    override fun onPostResume() {
-        super.onPostResume()
-        Log.v(tag, "[ ON POST RESUME ]")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.v(tag, "[ ON PAUSE ]")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.v(tag, "[ ON STOP ]")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.v(tag, "[ ON DESTROY ]")
-    }
+    override fun getLayout() = R.layout.activity_main
 
 }
