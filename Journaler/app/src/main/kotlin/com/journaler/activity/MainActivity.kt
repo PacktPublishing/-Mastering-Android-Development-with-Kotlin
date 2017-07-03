a326439847d8c580d0cd9735eefdb5f9c9c94199
Milos Vasic <milos85vasic@gmail.com>
2017-07-03 19:28:25 +0200

    Journaler: Fragments, switched to view pager.

--- Journaler/app/src/main/kotlin/com/journaler/activity/MainActivity.kt
+++ Journaler/app/src/main/kotlin/com/journaler/activity/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.journaler.activity
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
 import com.journaler.R
 import com.journaler.fragment.ItemsFragment
-import com.journaler.fragment.ManualFragment
-import kotlinx.android.synthetic.main.activity_header.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : BaseActivity() {
@@ -17,20 +19,16 @@ class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val fragment = ItemsFragment()
-        supportFragmentManager
-                .beginTransaction()
-                .add(R.id.fragment_container, fragment)
-                .commit()
+        pager.adapter = ViewPagerAdapter(supportFragmentManager)
+    }
 
-        filter_menu.setText(R.string.hlp)
-        filter_menu.setOnClickListener {
-            val userManualFrg = ManualFragment()
-            supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.fragment_container, userManualFrg)
-                    .addToBackStack("User manual")
-                    .commit()
+    private class ViewPagerAdapter(manager: FragmentManager) : FragmentStatePagerAdapter(manager) {
+        override fun getItem(position: Int): Fragment {
+            return ItemsFragment()
+        }
+
+        override fun getCount(): Int {
+            return 5
         }
     }
 
