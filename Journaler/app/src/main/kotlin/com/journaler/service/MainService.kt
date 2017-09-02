bc2d8523275cf00c38995f1c6fd579dc1589c64c
Milos Vasic <milos85vasic@gmail.com>
2017-09-02 17:52:45 +0200

    Journaler: Services, introducing main application service.

---
+++ Journaler/app/src/main/kotlin/com/journaler/service/MainService.kt
@@ -0,0 +1,34 @@
+package com.journaler.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+
+class MainService : Service() {
+
+    override fun onBind(p0: Intent?): IBinder {
+        TODO("not implemented")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        return super.onUnbind(intent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
