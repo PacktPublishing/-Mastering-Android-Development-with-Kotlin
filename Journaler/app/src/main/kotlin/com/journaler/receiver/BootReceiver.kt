ae6682c10dc8822e20e941f86293638c2e75fc1c
Milos Vasic <milos85vasic@gmail.com>
2017-09-14 09:19:14 +0200

    Journaler: Boot and shutdown receiver registered in manifest.

---
+++ Journaler/app/src/main/kotlin/com/journaler/receiver/BootReceiver.kt
@@ -0,0 +1,12 @@
+package com.journaler.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+
+    }
+}
---
+++ Journaler/app/src/main/kotlin/com/journaler/receiver/ShutdownReceiver.kt
@@ -0,0 +1,11 @@
+package com.journaler.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ShutdownReceiver : BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+
+    }
+}
