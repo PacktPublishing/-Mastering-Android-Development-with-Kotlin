84813ba3ce459a2386f62b14a88e890d1c10210e
Milos Vasic <milos85vasic@gmail.com>
2017-09-28 09:12:18 +0200

    Journaler: Content provider client application.

---
+++ Journaler/content_provider_client_example/src/main/kotlin/com/journaler/content_provider_client/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.journaler.content_provider_client
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        select.setOnClickListener {
+
+        }
+
+        insert.setOnClickListener {
+
+        }
+
+        update.setOnClickListener {
+
+        }
+
+        delete.setOnClickListener {
+
+        }
+    }
+
+}
