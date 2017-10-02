bbf26a1f8b4a1796b13723da25ea400b1af0d0f3
Milos Vasic <milos85vasic@gmail.com>
2017-10-02 09:58:58 +0200

    Journaler: Content provider, client app: update.

--- Journaler/content_provider_client_example/src/main/kotlin/com/journaler/content_provider_client/MainActivity.kt
+++ Journaler/content_provider_client_example/src/main/kotlin/com/journaler/content_provider_client/MainActivity.kt
@@ -61,7 +61,7 @@ class MainActivity : AppCompatActivity() {
                         location.latitude = x.toDouble()
                         location.longitude = x.toDouble()
                         values.put("location", gson.toJson(location))
-                        if(contentResolver.insert(uri, values) != null){
+                        if (contentResolver.insert(uri, values) != null) {
                             Log.v(
                                     tag,
                                     "Note inserted [ $x ]"
@@ -79,7 +79,49 @@ class MainActivity : AppCompatActivity() {
         }
 
         update.setOnClickListener {
-            // TODO: Implement.
+            val task = object : AsyncTask<Unit, Unit, Unit>() {
+                override fun doInBackground(vararg p0: Unit?) {
+                    val selection = StringBuilder()
+                    val selectionArgs = mutableListOf<String>()
+                    val uri = Uri.parse("content://com.journaler.provider/notes")
+                    val cursor = contentResolver.query(
+                            uri, null, selection.toString(), selectionArgs.toTypedArray(), null
+                    )
+                    while (cursor.moveToNext()) {
+                        val values = ContentValues()
+                        val id = cursor.getLong(cursor.getColumnIndexOrThrow("_id"))
+                        val titleIdx = cursor.getColumnIndexOrThrow("title")
+                        val title = "${cursor.getString(titleIdx)} upd: ${System.currentTimeMillis()}"
+                        val messageIdx = cursor.getColumnIndexOrThrow("message")
+                        val message = "${cursor.getString(messageIdx)} upd: ${System.currentTimeMillis()}"
+                        val locationIdx = cursor.getColumnIndexOrThrow("location")
+                        val locationJson = cursor.getString(locationIdx)
+                        values.put("_id", id)
+                        values.put("title", title)
+                        values.put("message", message)
+                        values.put("location", locationJson)
+
+                        val updated = contentResolver.update(
+                                uri, values, "_id = ?", arrayOf(id.toString())
+                        )
+                        if (updated > 0) {
+                            Log.v(
+                                    tag,
+                                    "Notes updated [ $updated ]"
+                            )
+                        } else {
+                            Log.e(
+                                    tag,
+                                    "Notes not updated"
+                            )
+                        }
+
+
+                    }
+                    cursor.close()
+                }
+            }
+            task.execute()
         }
 
         delete.setOnClickListener {
